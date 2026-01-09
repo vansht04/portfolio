50152594c687d6871ad450c8fe3bdcdf6ef95961
Vansh Tejnani <vanshbt@gmail.com>
2026-01-09 10:10:58 -0500

    bug fix

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -4,14 +4,14 @@ import Image from "next/image";
 export default function SocialLinks() {
   return (
     <div className="socials">
-      <a href="https://github.com/yourusername" target="_blank" rel="noopener noreferrer">
+      <a href="https://github.com/vansht04" target="_blank" rel="noopener noreferrer">
         <div className="icon-container">
-          <Image src="/github.svg" alt="GitHub" width={32} height={32} style={{ objectFit: "contain" }} />
+          <Image src="/github.png" alt="GitHub" width={32} height={32} style={{ objectFit: "contain" }} />
         </div>
       </a>
-      <a href="https://linkedin.com/in/yourusername" target="_blank" rel="noopener noreferrer">
+      <a href="https://linkedin.com/in/vansh-tejnani" target="_blank" rel="noopener noreferrer">
         <div className="icon-container">
-          <Image src="/linkedin.svg" alt="LinkedIn" width={32} height={32} style={{ objectFit: "contain" }} />
+          <Image src="/linkedin.png" alt="LinkedIn" width={32} height={32} style={{ objectFit: "contain" }} />
         </div>
       </a>
     </div>
